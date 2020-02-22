0866a2904cdb3837ee21a1ebcdd37012b01f608a
Andrew Hollabaugh <andrewhollabaugh12@gmail.com>
2020-02-22 18:10:06 -0500

    std register

--- register_nbit.v
+++ register_nbit.v
@@ -1,13 +1,13 @@
-module register_nbit(Q, D, l, r, clk);
+module register_nbit(clk, rst, l, Q, D);
 	parameter N = 8; // number of bits
-	output reg [N-1:0] Q; // registered output
-	input [N-1:0] D; // data input
-	input l; // load enable
-	input r; // positive logic asynchronous reset
 	input clk; // positive edge clock
+	input rst; // positive logic asynchronous reset
+	input l; // load enable
+	input [N-1:0] D; // data input
+	output reg [N-1:0] Q; // registered output
 	
-	always @(posedge clk or posedge r) begin
-		if(r)
+	always @(posedge clk or posedge rst) begin
+		if(rst)
 			Q <= 0;
 		else if(l)
 			Q <= D;
