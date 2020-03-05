c2dd60775772f051ef4cd9a63c2efe89f32262bb
Andrew Hollabaugh <andrewhollabaugh12@gmail.com>
2020-03-05 11:35:43 -0500

    moved verilog files to verilog folder

--- addr_detect.v
+++
@@ -1,8 +0,0 @@
-module addr_detect(addr, out);
-	input [7:0] addr;
-	output out;
-	parameter base_addr = 8'h00; // the address to detect
-	parameter addr_mask = 8'hFF; // which bits to care about (1 means we care)
-	
-	assign out = ((addr & addr_mask) == base_addr);
-endmodule
